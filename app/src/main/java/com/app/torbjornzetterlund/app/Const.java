64c8271d8c84e3cfb836351f18a973645358e9d9
tottaz <torbjorn.zetterlund@greenpeace.org>
2016-06-27 12:24:53 +0200

    Changed an API call for search

--- app/src/main/java/com/app/torbjornzetterlund/app/Const.java
+++ app/src/main/java/com/app/torbjornzetterlund/app/Const.java
@@ -18,7 +18,7 @@ public class Const {
     //    public static final String URL_RECENTLY_ADDED    = "http://www.torbjornzetterlund.com/api/get_recent_posts/";
     public static final String URL_CATEGORY_POST	 = "http://torbjornzetterlund.com/wp-json/wp/v2/posts?categories/_CAT_ID_/";
     public static final String URL_STORY_PAGE	 	 = "http://torbjornzetterlund.com/wp-json/wp/v2/posts/_STORY_ID_/";
-    public static final String URL_SEARCH_RESULT	 = "http://www.torbjornzetterlund.com/wp2android-api/v1/search/_SEARCH_KEYWORD_/_PAGE_NO_";
+    public static final String URL_SEARCH_RESULT	 = "http://www.torbjornzetterlund.com/wp-json/wp/v2/posts?search=_SEARCH_KEYWORD_&per_page=_PAGE_NO_";
     public static final String URL_COMMENTS_PAGE	    = "http://torbjornzetterlund.com/wp-json/wp/v2/comments?post_comment/_STORY_ID_/";
     public static final String URL_COMMENTS_LIST_PAGE   = "http://torbjornzetterlund.com/wp-json/wp/v2/comments?post/_STORY_ID_/_PAGE_NO_";
 
